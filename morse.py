495944af427559c0ca0242914ead8992d26217dd
John Comeau <jc@unternet.net>
2024-05-30 05:29:48 -0700

    translation to morse now works

--- morse.py
+++ morse.py
@@ -7,6 +7,7 @@ https://yetanotherprogrammingblog.medium.com/antlr-with-python-974c756bdb1b
 Some refactoring done by jc
 '''
 import sys, re, logging  # pylint: disable=multiple-imports
+from ast import literal_eval as eval
 from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker
 from MorseLexer import MorseLexer
 from MorseParser import MorseParser
@@ -22,10 +23,13 @@ NUMBERNAMES = [
     'ZERO', 'ONE', 'TWO', 'THREE', 'FOUR',
     'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE'
 ]
-NUMBERS = {
+NUMBERS = {  # this one is for the morse-to-text translator
     getattr(MorseParser, NUMBERNAMES[index]): str(index)
     for index in range(len(NUMBERNAMES))
 }
+NUMBERS.update({  # this one is for text-to-morse
+    name: str(NUMBERNAMES.index(name)) for name in NUMBERNAMES
+})
 SEPARATORS = {
     MorseParser.SENTENCE: '.',
     MorseParser.WORD: ' ',
@@ -76,16 +80,14 @@ def to_morse(string):
      to learn the use of ANTLR. just including this for completeness.)
     '''
     string = string.strip('"').lower()  # strip quotes and lowercase all
-    literals = MorseLexer.literalNames
-    symbols = MorseLexer.symbolicNames[:len(literals)]
+    # skip <INVALID> entries in both literals and symbols
+    literals = [eval(l) for l in MorseLexer.literalNames[1:]]
+    symbols = MorseLexer.symbolicNames[1:len(literals) + 1]
     for symbol in symbols:
         if len(symbol) == 1:
             symbols[symbols.index(symbol)] = symbol.lower()
         else:
-            try:
-                symbols[symbols.index(symbol)] = NUMBERS[symbol]
-            except KeyError:  # disregard error on <INVALID>
-                pass
+            symbols[symbols.index(symbol)] = NUMBERS[symbol]
     translator = dict(zip(symbols, literals))
     for sentence in string.split('.'):
         for word in sentence.split():
@@ -93,6 +95,7 @@ def to_morse(string):
                 print(translator[letter], end=' ')
             print('  ', end='')  # extra two spaces at end of word
         print('   ', end='')  # extra 3 spaces at end of sentence
+    print()  # final end-of-line
 
 def dispatch(string):
     '''
