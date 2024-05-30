0be56e18d0053281a1961b125aabff305689146f
John Comeau <jc@unternet.net>
2024-05-30 05:09:09 -0700

    got rid of all print statements except for final result

--- jsparse.py
+++ jsparse.py
@@ -6,36 +6,31 @@ https://github.com/antlr/grammars-v4/tree/master/javascript/javascript/Python3
 '''
 import sys, logging  # pylint: disable=multiple-imports
 from antlr4 import *
-import JavaScriptLexer
-import JavaScriptParser
+from JavaScriptLexer import JavaScriptLexer as JSL
+from JavaScriptParser import JavaScriptParser as JSP
 
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
-JSL = JavaScriptLexer.JavaScriptLexer
-JSP = JavaScriptParser.JavaScriptParser
-
 class WriteTreeListener(ParseTreeListener):
     def visitTerminal(self, node:TerminalNode):
-        print ("Visit Terminal: " + str(node) + " - " + repr(node))
+        logging.info("Visit Terminal: " + str(node) + " - " + repr(node))
 
 def main(argv):
     input_stream = FileStream(argv[1])
-    print("Test started for: " + argv[1])
+    logging.info("Test started for: %s", argv[1])
     lexer = JSL(input_stream)
     stream = CommonTokenStream(lexer)
     parser = JSP(stream)
-    print("Created parsers")
+    logging.info("Created parsers")
     tree = parser.program()
     logging.info('walking parse tree')
     ParseTreeWalker.DEFAULT.walk(WriteTreeListener(), tree)
-    logging.info('parse tree:')
-    print(tree.toStringTree(recog=parser))
-    logging.info('token list')
+    logging.info('parse tree: %s', tree.toStringTree(recog=parser))
     input_stream.seek(0)
     lexer = JSL(input_stream)
     logging.info('reconstructed:')
     print(''.join([token.text for token in lexer.getAllTokens()]))
 
 if __name__ == '__main__':
-    print("Running")
+    logging.info("Running")
     main(sys.argv)
