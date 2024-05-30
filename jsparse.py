c42df4d1a587ff1e181bb8132bc218b72ce6b4d3
John Comeau <jc@unternet.net>
2024-05-30 14:58:55 -0700

    still trying to figure out how to use token rewriter

--- jsparse.py
+++ jsparse.py
@@ -5,21 +5,33 @@ Example JavaScript parser from ANTLR repository
 https://github.com/antlr/grammars-v4/tree/master/javascript/javascript/Python3
 '''
 import sys, logging  # pylint: disable=multiple-imports
-from antlr4 import *
+from antlr4 import FileStream, CommonTokenStream, TerminalNode, \
+    ParseTreeListener, ParseTreeWalker
+from antlr4.TokenStreamRewriter import TokenStreamRewriter
 from JavaScriptLexer import JavaScriptLexer as JSL
 from JavaScriptParser import JavaScriptParser as JSP
 
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 class WriteTreeListener(ParseTreeListener):
+    '''
+    Subclass of ParseTreeListener to write out parse tree
+    '''
     def visitTerminal(self, node:TerminalNode):
-        logging.info("Visit Terminal: " + str(node) + " - " + repr(node))
+        '''
+        Shows terminal nodes visited to stderr
+        '''
+        logging.info("Visit Terminal: %s - %s", node, repr(node))
 
-def main(argv):
-    input_stream = FileStream(argv[1])
-    logging.info("Test started for: %s", argv[1])
+def main(filename):
+    '''
+    Parse file and display it, optionally modifying it
+    '''
+    input_stream = FileStream(filename)
+    logging.info("Test started for: %s", filename)
     lexer = JSL(input_stream)
     stream = CommonTokenStream(lexer)
+    rewriter = TokenStreamRewriter(stream)
     parser = JSP(stream)
     listener = WriteTreeListener()
     logging.info("Created parsers")
@@ -31,7 +43,7 @@ def main(argv):
     lexer = JSL(input_stream)
     logging.info('reconstructed:')
     print(''.join([token.text for token in lexer.getAllTokens()]))
+    logging.debug(dir(rewriter))
 
 if __name__ == '__main__':
-    logging.info("Running")
-    main(sys.argv)
+    main(sys.argv[1])
