bf00a20635a18cfeef8a9851b3da077f9825b66e
John Comeau <jc@unternet.net>
2024-06-02 10:32:15 -0700

    went back to bentrevett's last update

--- Makefile
+++
@@ -1,4 +0,0 @@
-run: testdelete.py Java9Lexer.py
-	python3 $<
-Java9Lexer.py: Java9.g4
-	antlr4 -Dlanguage=Python3 $<
--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,5 @@
 public class HelloWorld { 
    public static void main(String[] args) { 
-      /* Let's get
-       * rid of
-       * this comment
-       * using antlr4 */
       System.out.println("Hello, World");
    }
 }
