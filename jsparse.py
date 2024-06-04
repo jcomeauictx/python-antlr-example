8d196f2c16e3567c3d4ec3c356d448ed980d3b60
John Comeau <jc@unternet.net>
2024-06-04 14:59:41 -0700

    copied over better jsfix.py from legaproxy project

--- jsparse.py
+++ jsparse.py
@@ -6,13 +6,11 @@ adapted from sample script at
 https://github.com/antlr/grammars-v4/tree/master/javascript/javascript/Python3
 '''
 import sys, logging  # pylint: disable=multiple-imports
-from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
+from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker
 from antlr4.TokenStreamRewriter import TokenStreamRewriter
 from JavaScriptLexer import JavaScriptLexer
 from JavaScriptParser import JavaScriptParser
 from JavaScriptParserListener import JavaScriptParserListener
-
-logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 LOWERCASE_LETTERS = tuple('abcdefghijklmnopqrstuvwxyz')
 
@@ -28,7 +26,7 @@ class DowngradingJavascriptListener(JavaScriptParserListener):
         '''
         self.rewriter = rewriter
 
-    def enterVariableDeclarationList(self, ctx):
+    def exitVariableDeclarationList(self, ctx):
         '''
         convert `let` and `const` to `var`
         '''
@@ -40,18 +38,26 @@ class DowngradingJavascriptListener(JavaScriptParserListener):
                 modifier.start, modifier.stop, 'var'
             )
 
-    def enterArrowFunction(self, ctx):
+    def exitArrowFunction(self, ctx):
         '''
         convert arrow function to old-style `function(){;}`
         '''
         logging.debug('ctx: %r: %s', ctx.getText(), show(ctx))
-        parameters, arrow, body = ctx.children
-        logging.debug('ctx.arrowFunctionParameters: %s',
-                      parameters.getText())
-        logging.debug('ctx.arrowFunctionBody: %s',
-                      body.getText())
+        parameters = ctx.arrowFunctionParameters()
+        body = ctx.arrowFunctionBody()
+        arrow = ([child.symbol for child in ctx.children
+                 if getattr(child, 'symbol', None) is not None
+                 and child.symbol.text == '=>'] + [None])[0]
+        logging.debug('ctx.arrowFunctionParameters: %s: %s`',
+                      parameters, show(parameters))
+        logging.debug('ctx.arrowFunctionBody: %s: %s',
+                      body, show(body))
         #import pdb; pdb.set_trace()
-        self.rewriter.deleteToken(arrow.symbol)
+        if arrow is not None:
+            logging.debug('arrow: %s: %s', arrow, show(arrow))
+            self.rewriter.deleteToken(arrow)
+        else:
+            logging.error('no arrow was parsed, likely an antlr4 error node')
         if parameters.start.text != '(':
             # assume single unparenthesized arg
             self.rewriter.insertBeforeToken(parameters.start, 'function(')
@@ -64,11 +70,11 @@ class DowngradingJavascriptListener(JavaScriptParserListener):
             self.rewriter.insertBeforeToken(body.start, '{return ')
             self.rewriter.insertAfterToken(body.stop, '}')
 
-def main(filename):
+def fixup(filedata):
     '''
-    Parse file, fix for older browsers, and print out modified source
+    Parse data, fix for older browsers, and return modified source
     '''
-    input_stream = FileStream(filename)
+    input_stream = InputStream(filedata)
     lexer = JavaScriptLexer(input_stream)
     tokens = CommonTokenStream(lexer)
     parser = JavaScriptParser(tokens)
@@ -79,7 +85,7 @@ def main(filename):
     walker.walk(listener, tree)
     logging.log(logging.NOTSET,  # change to logging.DEBUG to see this
         'parse tree: %s', tree.toStringTree(recog=parser))
-    print(listener.rewriter.getDefaultText())
+    return listener.rewriter.getDefaultText()
 
 def show(something):
     '''
@@ -98,4 +104,12 @@ def show(something):
     return result
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
+    if len(sys.argv) > 1:
+        for filename in sys.argv[1:]:
+            with open(filename, 'r', encoding='utf-8') as infile:
+                print(fixup(infile.read()))
+    else:
+        logging.error('Usage: %s filename.js', sys.argv[0])
+        logging.warning('assuming data on stdin, ^D or ^C if necessary')
+        print(fixup(sys.stdin.read()))
